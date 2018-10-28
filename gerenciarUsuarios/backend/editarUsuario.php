4444c1a93ca91d34c9ae79f08062157821d98c35
Willian Duarte <willian_xzz@hotmail.com>
2018-10-27 23:17:04 -0200

    Foi adicionado o campo email ao editar que estava faltando e foi desenvolvido a proteção para salvar a senha corretamente.

--- gerenciarUsuarios/backend/editarUsuario.php
+++ gerenciarUsuarios/backend/editarUsuario.php
@@ -4,6 +4,8 @@ session_start();
 
 if(isset($_SESSION['logado']) && $_SESSION['logado'] === true){
 	
+	
+	
 	 $id =   $_POST['idEditar'];
 	 $nome = $_POST['nome'.$id];
      $sobrenome = $_POST['sobrenome'.$id];
@@ -11,6 +13,7 @@ if(isset($_SESSION['logado']) && $_SESSION['logado'] === true){
 	 $senhaEditar = md5($_POST['senhaEditar'.$id]);
 	 $tipo = $_POST['tipo'.$id];
 	 $ativo = $_POST['ativo'.$id];
+	 
 	
 	// Create connection
 	$conectado = include("conectdb.php");				
@@ -21,13 +24,25 @@ if(isset($_SESSION['logado']) && $_SESSION['logado'] === true){
 			die("Connection failed: " . mysqli_connect_error());
 		}
 		
+		//Se não for um md5:
+         if (strlen($_POST['senhaEditar'.$id]) !== 32 ) { //Salve a senha.
+			 $sql = "UPDATE usuario SET ".
+			"nome='".$nome."',".
+			"sobrenome='".$sobrenome."',".
+			"email='".$email."',".
+			"senha='".$senhaEditar."',".
+			"tipo='".$tipo."',".
+			"ativo='".$ativo."' WHERE idusuario=".$id;
+	    }else{
+			$sql = "UPDATE usuario SET ".  //Não salve a senha.
+			"nome='".$nome."',".
+			"sobrenome='".$sobrenome."',".
+			"email='".$email."',".
+			"tipo='".$tipo."',".
+			"ativo='".$ativo."' WHERE idusuario=".$id;
+		}
 		
-		$sql = "UPDATE usuario SET ".
-		"nome='".$nome."',".
-		"sobrenome='".$sobrenome."',".
-		"senha='".$senhaEditar."',".
-		"tipo='".$tipo."',".
-		"ativo='".$ativo."' WHERE idusuario=".$id;
+		
 		$result = mysqli_query($conectado, $sql) or die (mysqli_error($conectado));
 		
 		mysqli_close($conectado);
@@ -39,8 +54,9 @@ if(isset($_SESSION['logado']) && $_SESSION['logado'] === true){
 			header("location: ../screen/painel.php?editado=0");
 			exit;
 		}		
-	}
 	
+	
+  } 
 	
 }else{
 	header("localtion: ../index.html"); 
