f8ad925598cb096afefc66957c60468096d33295
Willian Duarte <willian_xzz@hotmail.com>
2018-10-29 22:50:14 -0200

    Foi desenvolvido mais funções de segurança de inputs e aplicadoaos mesmos.

--- gerenciarUsuarios/backend/editarUsuario.php
+++ gerenciarUsuarios/backend/editarUsuario.php
@@ -5,58 +5,83 @@ session_start();
 $administradorLogado = isset($_SESSION['logado']) && $_SESSION['logado'] === true && $_SESSION['tipo'] == "Administrador";
 if($administradorLogado){
 	
-	
-	 $id =   $_POST['idEditar'];
-	 $nome = $_POST['nome'.$id];
-     $sobrenome = $_POST['sobrenome'.$id];
-	 $email = $_POST['email'.$id];
-	 $senhaEditar = md5($_POST['senhaEditar'.$id]);
-	 $tipo = $_POST['tipo'.$id];
-	 $ativo = $_POST['ativo'.$id];
+	 include("funcoes.php");
+			
+	 $valido = true;
 	 
-	
-	// Create connection
-	$conectado = include("conectdb.php");				
+     $id =   $_POST['idEditar'];	 
+	 $inputsEsperados = array();
+	 $inputsEsperados[0] = $_POST['nome'.$id];
+	 $inputsEsperados[1] = $_POST['sobrenome'.$id];
+	 $inputsEsperados[2] = $_POST['email'.$id];
+	 $inputsEsperados[3] = $_POST['senhaEditar'.$id];
+	 $inputsEsperados[4] = $_POST['tipo'.$id];
+	 $ativo = $_POST['ativo'.$id];
 		
-	if($conectado){					
-		// Check connection
-		if (!$conectado) {
-			die("Connection failed: " . mysqli_connect_error());
+
+		
+	 if($ativo !== 0 || $ativo !== 1){
+		$valido = false;
+	 }
+	
+	 for($i = 0; $i < count($inputsEsperados); $i++){
+		$valido = verificarInput($inputsEsperados[$i]);
+		if($valido === false){
+			break;
 		}
+	 }
+	 
+	 if($valido){	
+	 
+		 
+		 $nome = $inputsEsperados[0];
+		 $sobrenome = $inputsEsperados[1];
+		 $email = $inputsEsperados[2];
+		 $senhaEditar = md5($inputsEsperados[3]);
+		 $tipo = $inputsEsperados[4];
+		 
 		
-		//Se não for um md5(encriptografada):
-         if (strlen($_POST['senhaEditar'.$id]) !== 32 ) { //Salve a senha.
-			 $sql = "UPDATE usuario SET ".
-			"nome='".$nome."',".
-			"sobrenome='".$sobrenome."',".
-			"email='".$email."',".
-			"senha='".$senhaEditar."',".
-			"tipo='".$tipo."',".
-			"ativo='".$ativo."' WHERE idusuario=".$id;
-	    }else{
-			$sql = "UPDATE usuario SET ".  //Não salve a senha.
-			"nome='".$nome."',".
-			"sobrenome='".$sobrenome."',".
-			"email='".$email."',".
-			"tipo='".$tipo."',".
-			"ativo='".$ativo."' WHERE idusuario=".$id;
-		}
-		
-		
-		$result = mysqli_query($conectado, $sql) or die (mysqli_error($conectado));
-		
-		mysqli_close($conectado);
-		
-		if($result){
-			header("location: ../screen/painelAdministrador.php?editado=1");
-			exit;
-		}else{
-			header("location: ../screen/painelAdministrador.php?editado=0");
-			exit;
-		}		
-	
-	
-  } 
+		// Create connection
+		$conectado = include("conectdb.php");				
+			
+		if($conectado){					
+			// Check connection
+			if (!$conectado) {
+				die("Connection failed: " . mysqli_connect_error());
+			}
+			
+			//Se não for um md5(encriptografada):
+			 if (strlen($_POST['senhaEditar'.$id]) !== 32 ) { //Salve a senha.
+				 $sql = "UPDATE usuario SET ".
+				"nome='".$nome."',".
+				"sobrenome='".$sobrenome."',".
+				"email='".$email."',".
+				"senha='".$senhaEditar."',".
+				"tipo='".$tipo."',".
+				"ativo='".$ativo."' WHERE idusuario=".$id;
+			}else{
+				$sql = "UPDATE usuario SET ".  //Não salve a senha.
+				"nome='".$nome."',".
+				"sobrenome='".$sobrenome."',".
+				"email='".$email."',".
+				"tipo='".$tipo."',".
+				"ativo='".$ativo."' WHERE idusuario=".$id;
+			}
+			
+			
+			$result = mysqli_query($conectado, $sql) or die (mysqli_error($conectado));
+			
+			mysqli_close($conectado);
+			
+			if($result){
+				header("location: ../screen/painelAdministrador.php?editado=1");
+				exit;
+			}else{
+				header("location: ../screen/painelAdministrador.php?editado=0");
+				exit;
+			}
+		  } 
+       }
 	
 }else{
 	header("localtion: ../index.html"); 
--- gerenciarUsuarios/backend/funcoes.php
+++ gerenciarUsuarios/backend/funcoes.php
@@ -3,10 +3,40 @@ header('Content-type: text/html; charset=UTF-8');
 function verificarInput($inputString){
 	$ok = false;
 	
-	if(isset($inputString) && !empty($inputString)){
+	$inputSeguro = RemoverAspas($inputString);
+	$inputSeguro = RemoverNocivos($inputString);
+	$inputSeguro = RemoverHtml($inputString);
+	
+	
+	if(isset($inputSeguro) && !empty($inputSeguro)){
 	 $ok = true;
     }
 	return $ok;
+}
+
+//Funções de uso interno.
+function RemoverAspas($campo){
+return addslashes($campo); //remove as aspas duplas/simples
+}
+
+function RemoverNocivos($campo){
+return htmlspecialchars($campo);// remove aspas simples/duplas,>,<,&
+}
+
+function RemoverHtml($campo){
+return strip_tags($campo); // remove todas as tags html
+}
+
+function RemoverHtml2($campo,$excesao){ // remove todas as tags html com excesão das que são passadas pelo parametro.
+return strip_tags($campo,$excesao);
+}
+
+function DeixarMinusculas($campo){
+return strtolower($campo); // deixa o texto em minusculo
+}
+
+function DeixarMaiusculas($campo){
+return strtoupper($campo); // deixa o texto em MAIUSCULO
 }
 
 
